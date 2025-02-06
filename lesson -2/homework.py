3aacb61bace65e49b316826e0ec2a238ba8d0b96
xazratqulovtemur <khazratkulovtemur@gmail.com>
2025-02-06 22:11:28 +0500

    update

--- lesson -2/homework.py
+++ lesson -2/homework.py
@@ -207,5 +207,8 @@ word=str(input("Enter the sentence: "))
 words=word.split()
 print(f"Sentence begin with {words[0]}, and ends with {words[-1]}")
 """
-
+#ftgjhbkl
+#ruvhebnvqmv
 #bool problems
+
+#problem1
