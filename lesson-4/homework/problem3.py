a828c591fe038605ab673dd14f67bffb701e499b
xazratqulovtemur <khazratkulovtemur@gmail.com>
2025-02-14 10:31:12 +0500

    all done

--- lesson-4/homework/problem3.py
+++ lesson-4/homework/problem3.py
@@ -1,59 +1,19 @@
-"""
-txt=str(input("Enter the text: "))
-new_txt=''
+txt='abcabcdabcdeabcdefabcdefg'
 
+done={'a', 'e', 'i', 'o', 'u'}
 
+counter=0
 
-seen_chars=['a', 'b', 'c', 'e', 'i', 'A', 'I', 'U', 'O', 'E']
-
-
-count=0
-valid_count=0
+ans=""
 
 for i in range(len(txt)):
-    if txt[i] not in seen_chars: 
-        valid_count+=1
-        new_txt+=txt[i]
-
-    if txt[i] in seen_chars:
-        pass
-        
-    if valid_count%3==0 and valid_count!=0:
-        seen_chars.append(txt[i])
-        new_txt+='_'
-        valid_count+=1
-    
-print(new_txt)
-
-"""
-
-txt = input("Enter the text: ")
-new_txt = ''
- 
-
-seen_chars = ['a', 'b', 'c', 'e', 'i', 'A', 'I', 'U', 'O', 'E']
-pending_underscore = False
-valid_count = 0
-
-for i in range(len(txt)):
-    if txt[i] in seen_chars: 
-      new_txt += txt[i]
-      continue
-    if pending_underscore:
-      new_txt += '_'
-      pending_underscore = False
-
-    valid_count += 1
-    new_txt +=txt[i]
-    if valid_count % 3 == 0:
-      new_txt += '_'
-      
-      pending_underscore = True
-    
-print(new_txt)
+  counter+=1
+  ans +=txt[i]
+  if i!=len(txt)-1 and counter >= 3 and txt[i] not in done:
+    ans += '_'
+    done.add(txt[i])
+    counter=0
 
 
-
-
-    
-
+print(ans)
+     
